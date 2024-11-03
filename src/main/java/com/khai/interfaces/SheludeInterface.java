48c256c2c3e4d54beec817b92b2fd47b4187f3a5
Vladislav <chvs1312@gmail.com>
2024-11-03 15:58:23 +0200

    create SheludeInterface.java

---
+++ src/main/java/com/khai/interfaces/SheludeInterface.java
@@ -0,0 +1,7 @@
+package com.khai.interfaces;
+
+public interface SheludeInterface {
+    void setSchedule(DeviceInterface deviceInterface, String time);
+    void removeSchedule(DeviceInterface deviceInterface);
+    void viewSchedules();
+}
